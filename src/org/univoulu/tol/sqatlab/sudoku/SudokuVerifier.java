f75613577f5a0e634846f28b4b4748d965afd233
Silvergrail <ulfest@hotmail.com>
2015-10-01 13:46:50 +0300

    FirstCommit
    The first commit with the test cases set up

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -7,24 +7,20 @@ import java.util.Scanner;
 public class SudokuVerifier {
 	
 
-	private static List<Integer> numberList = new ArrayList<Integer>();
+	private List<Integer> numberList = new ArrayList<Integer>();
 	private static String validAnswer = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 	private static String invalidAnswer = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
 
 	public int verify(String candidateSolution) {
 		
-		Scanner scanner = new Scanner(candidateSolution);
-		
-		List<Integer> intList = null;
-		
-		while (scanner.hasNextInt()) {
-		    numberList.add(scanner.nextInt());
-		    
-		System.out.println(numberList);
+		for (char temp : candidateSolution.toCharArray()) {
+			numberList.add(Integer.valueOf(String.valueOf(temp)));
+						
 		}
+		System.out.println(numberList);
 		
 		
-		// returns 1 if the candidate solution is correct
+		// returns 0 if the candidate solution is correct
 		return 0;
 	}
 	
--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -11,12 +11,12 @@ public class TestSudokuVerifier {
 	
 	private static String validAnswer = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 	private static String invalidAnswer = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
-	
+	SudokuVerifier verifier;
 	
 	@Before
 	public void setup() {
 		
-		SudokuVerifier verifier = new SudokuVerifier();
+		verifier = new SudokuVerifier();
 		
 		
 	}
